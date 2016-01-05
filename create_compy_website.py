68a3ac1afb22249f2f6c620aa58617968aa46b8a
Juan José Denis Corrales <jjdenis@gmail.com>
2016-01-05 16:46:01 +0100

    Renamed docs to website

--- create_compy_website.py
+++ create_compy_website.py
@@ -7,4 +7,4 @@ from create_website.create import make_all_html
 
 make_all_html()
 
-print webbrowser.open('file:////Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/docs/challenges.html')
+print webbrowser.open('file:////Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/website/challenges.html')
--- create_website/settings.py
+++ create_website/settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Two modes of operation, if True, it helps with docs
+# Two modes of operation, if True, it helps with website
 # this is for development.
 # If false, the screenshots will be in the base path.
 # This is for use after pip install
@@ -9,8 +9,8 @@ from jinja2 import Environment, PackageLoader
 
 SAVE_TO_DOCS_PATH = True
 
-DOCS_PATH   = u'/Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/docs/'
-SSHOTS_PATH = u'/Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/docs/img/'
+DOCS_PATH   = u'/Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/website/'
+SSHOTS_PATH = u'/Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/website/img/'
 CODE_PATH   = u'/Users/jjdenis/Dropbox/Familia/Programas/ProyectoCompy/web_compy/examples/'
 
 jinja_environment_templates = Environment(loader=PackageLoader('create_website', 'templates'))
